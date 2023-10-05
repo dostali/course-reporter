6842f120d3a6ba0ea814c38e5600bf5857e2b382
Mikayil Nurmammadov <16880120-Shekilee@users.noreply.gitlab.com>
2023-10-05 17:18:24 +0400

    created CourseTeacherContract service

--- CourseReporter/src/main/java/az/bdc/courserepoter/constant/SqlCommands.java
+++ CourseReporter/src/main/java/az/bdc/courserepoter/constant/SqlCommands.java
@@ -3,8 +3,7 @@ package az.bdc.courserepoter.constant;
 public final class SqlCommands {
 
 
-
-    public static class Course{
+    public static class Course {
         public final static String SELECT_ALL = "select * from course c";
         public final static String INSERT_INTO = "insert into course(name, address, phone_number, create_date, update_date) values(?,?,?,?,?)";
         public final static String DELETE_BY_ID = "delete from course where id=?";
@@ -16,12 +15,24 @@ public final class SqlCommands {
         public final static String SELECT_ALL = "select * from teacher ";
         public final static String INSERT_INTO = "insert into teacher (name,surname,phone_number,create_date,update_date) values (?,?,?,?,?)";
         public final static String DELETE_BY_ID = "delete from teacher where id=?";
-        public final static String UPDATE_SET = "update teacher name=?,surname=?,phone_number=?,update_date=? where id=?)";
+        public final static String UPDATE_SET = "update teacher (name=?,surname=?,phone_number=?,update_date=?) where id=?)";
     }
 
-    static class Students{
-
+    public static class CourseTeacherContract {
+        public final static String SELECT_ALL = "select * from course_teacher_contract ";
+        public final static String INSERT_INTO = "insert into course_teacher_contract (total_amount,monthly_amount,course_id,teacher_id,create_date,update_date) values(?,?,?,?,?,?) ";
+        public final static String DELETE_BY_ID = "delete from course_teacher_contract where id=?";
+        public final static String UPDATE_SET = "update course_teacher_contract (total_amount=?,monthly_amount=?,course_id=?,teacher_id=?,create_date=?,update_date=?) where id=?)";
     }
 
+    public static class TeacherMonthlySalary {
+        public final static String SELECT_ALL = "select * from teacher_monthly_payment ";
+        public final static String INSERT_INTO = "insert into teacher_monthly_payment (amount,create_date,update_date,month,contract_id) values(?,?,?,?,?) ";
+        public final static String DELETE_BY_ID = "delete from teacher_monthly_payment where id=?";
+        public final static String UPDATE_SET = "update teacher_monthly_payment (amount=?,update_date=?,month=?,contract_id=?) where id=?)";
 
+        static class Students {
+
+        }
+    }
 }
